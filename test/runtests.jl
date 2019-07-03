0f5a5f7f7771e6505724c43852de48b57d748370
Paulito Palmes <ppalmes@gmail.com>
2019-07-03 16:45:26 +0100

    test updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,15 @@ using Test
 
 include("test_readerwriter.jl")
 
-#if LIB_SKL_AVAILABLE
-#    include("test_scikitlearn.jl")
-#else
-#    @info "Skipping scikit-learn tests."
-#end
-#if LIB_CRT_AVAILABLE
-#    include("test_caret.jl")
-#else
-#    @info "Skipping CARET tests."
-#end
+if LIB_SKL_AVAILABLE
+    include("test_scikitlearn.jl")
+else
+    @info "Skipping scikit-learn tests."
+end
+if LIB_CRT_AVAILABLE
+    include("test_caret.jl")
+else
+    @info "Skipping CARET tests."
+end
 
 end
