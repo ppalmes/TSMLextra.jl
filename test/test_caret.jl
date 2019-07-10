3d827d0abc086305dcbe03995cd3785f1772992c
Paulito Palmes <ppalmes@gmail.com>
2019-07-10 16:34:45 +0100

    remove xgbTree in test

--- test/test_caret.jl
+++ test/test_caret.jl
@@ -17,10 +17,11 @@ const Y = IRIS[:,5] |> Vector
 const XX = IRIS[:,1:1] |> Matrix
 const YY = IRIS[:,4] |> Vector
 
-const learners=["rf","treebag","svmRadialWeights","pls","svmLinear","bagFDA","rpart","xgbTree"]
+const learners=["rf","treebag","svmRadialWeights","pls","svmLinear","bagFDA","rpart"]
 
 function test_caret_fit(learner::String)
     #crt = CaretLearner(Dict(:learner=>learner,:fitControl=>"trainControl(method='cv')"))
+    @info learner
     crt = CaretLearner(Dict(:learner=>learner))
     fit!(crt,X,Y)
     @test crt.model != nothing
@@ -28,6 +29,7 @@ end
 
 function test_caret_transform(learner::String)
     #crt = CaretLearner(Dict(:learner=>learner,:fitControl=>"trainControl(method='cv')"))
+    @info learner
     crt = CaretLearner(Dict(:learner=>learner))
     fit!(crt,X,Y)
     @test sum(transform!(crt,X) .== Y)/length(Y) > 0.80
