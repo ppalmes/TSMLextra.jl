fa9e859f0d5c276152935b187bae09a6d0cf6081
Paulito Palmes <ppalmes@gmail.com>
2019-08-15 11:02:21 +0100

    add e1071 during build

--- deps/build.jl
+++ deps/build.jl
@@ -31,7 +31,7 @@ end
 function installrml()
 	#packages=["caret", "earth","mda","e1071","gam","randomForest","nnet","kernlab","grid","MASS","pls"]
 	#packages=["caret", "e1071","gam","randomForest"]
-	packages=["caret"]
+	packages=["caret","e1071"]
 	for pk in packages
 		installrpackage(pk)
 	end
